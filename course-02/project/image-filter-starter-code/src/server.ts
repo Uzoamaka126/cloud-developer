67342be671fb5128d91f486d232aaaa7dedfba0a
Uzoamaka126 <vanessapeters126@gmail.com>
2020-07-21 22:36:09 +0100

    Add image filter function

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,6 +1,7 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import validator from 'validator';
 
 (async () => {
 
@@ -16,8 +17,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
+  // IT SHOULD:
   //    1. validate the image_url query
   //    2. call filterImageFromURL(image_url) to filter the image
   //    3. send the resulting file in the response
@@ -28,7 +28,35 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
+  app.get('/filteredimage', async (req: Request, res: Response, next) => {
+    const imageUrl:string = req.query.image_url
+    if (!imageUrl) {
+      return res.status(400).send({ "message": "No URL was specified in the query string" })
+    }
+    // validate image query
+    if (!validator.isURL(imageUrl, { allow_underscores: true })) {
+      return res.status(400).send({ "message": "Invalid URL" })
+    }
+    const authorizedExtensions = ['jpg', 'jpeg', 'svg', 'png', 'bmp', 'gif'];
 
+    if (!authorizedExtensions.includes(imageUrl.split('.')[imageUrl.split('.').length - 1])) {
+      return res.status(400).send({ "message": "Not an image or extension missing" })
+    };
+
+    // Get image from given URL, filter and save it locally
+    let image_path:string;
+
+    try {
+      image_path = await filterImageFromURL(imageUrl);
+    } catch(e) {
+      return res.status(500).send({ message: `Failed to process image: ${e.message}` });
+    }
+
+    // return filtered image to user
+    return res.sendFile(image_path, (err) => {
+      deleteLocalFiles([image_path]);
+    });
+  })
   //! END @TODO1
   
   // Root Endpoint
--- course-02/project/image-filter-starter-code/src/util/util.ts
+++ course-02/project/image-filter-starter-code/src/util/util.ts
@@ -1,5 +1,7 @@
+import { Jimp } from 'jimp/es';
 import fs from 'fs';
-import Jimp = require('jimp');
+const Jimp = require('jimp');
+import path from 'path';
 
 // filterImageFromURL
 // helper function to download, filter, and save the filtered image locally
@@ -9,19 +11,24 @@ import Jimp = require('jimp');
 // RETURNS
 //    an absolute path to a filtered image locally saved file
 export async function filterImageFromURL(inputURL: string): Promise<string>{
-    return new Promise( async resolve => {
-        const photo = await Jimp.read(inputURL);
-        const outpath = '/tmp/filtered.'+Math.floor(Math.random() * 2000)+'.jpg';
-        await photo
-        .resize(256, 256) // resize
-        .quality(60) // set JPEG quality
-        .greyscale() // set greyscale
-        .write(__dirname+outpath, (img)=>{
-            resolve(__dirname+outpath);
-        });
+    return new Promise( async (resolve, reject) => {
+        const image_name:string = 'filtered.'+ Math.floor(Math.random() * 2000) +'.jpg';
+        const image_path:string = path.join(__dirname, 'tmp', image_name);
+
+        try {
+            const photo:Jimp = await Jimp.read(inputURL);
+            photo
+            .resize(256, 256) // resize
+            .quality(60) // set JPEG quality
+            .greyscale() // set greyscale
+            .write(image_path, (img) => {
+                resolve(image_path);
+            })
+        } catch(e) {
+            reject(e)
+        }
     });
 }
-
 // deleteLocalFiles
 // helper function to delete files on the local disk
 // useful to cleanup after tasks
